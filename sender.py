45272b66699dc141bb66e0679464a7b951fbd0f8
Philip O'Toole <philip.otoole@yahoo.com>
2016-09-24 16:03:02 -0700

    Looks good

--- sender.py
+++ sender.py
@@ -11,6 +11,13 @@ SENDER = "Philip O'Toole <philip@percolate.com>"
 SUBJECT = 'Data Platform, Analytics, and Search role at Percolate'
 
 
+def yes_or_no(question):
+    reply = str(raw_input(question+' (y/N): ')).lower().strip()
+    if reply != '' and reply[0] == 'y':
+        return True
+    return False
+
+
 def parse_args():
     # Create default credential path.
     home_dir = os.path.expanduser('~')
@@ -19,14 +26,17 @@ def parse_args():
 
     parser = argparse.ArgumentParser(description='Send some emails')
     parser.add_argument('recipients', metavar='R', type=str,
-                        help='Comma-seperated file of name,email,message-ref')
+                        help='comma-seperated file of name,email,message-ref')
     parser.add_argument('messages', metavar='C', type=str,
                         help='|-seperated file of message-ref,message')
     parser.add_argument('footer', metavar='F', type=str,
-                        help='Footer file')
+                        help='footer file')
     parser.add_argument('--creds', metavar='FILE', type=str,
                         default=credential_path,
                         help='path to credentials file, default: %s' % credential_path)
+    parser.add_argument('--force', action="store_true",
+                        default=False,
+                        help='send emails without asking for confirmation')
     args = parser.parse_args()
     return args
 
@@ -48,7 +58,14 @@ def main():
     print '%d recipients loaded, %d messages loaded.' % (lr, lm)
 
     for m in composer:
-        print m
+        print '\n=========================================================\n\n'
+        if not args.force:
+            print m
+            if not yes_or_no('Send email?'):
+                continue
+
+        agent.send(m)
+        print 'Email sent to %s' % m.to
 
     composer.close()
 
